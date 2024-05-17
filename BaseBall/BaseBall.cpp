8fd2f5626563b3cae308911a1ab63646c1cb2a1d
seungpyo.hong <hsp8405@naver.com>
2024-05-17 14:25:29 +0900

    [refactoring] 42

--- BaseBall/BaseBall.cpp
+++ BaseBall/BaseBall.cpp
@@ -6,17 +6,26 @@ using namespace std;
 class BaseBall {
 public:
 	void guess(const string& guessNum) {
+		assertIllegalArgument(guessNum);
+	}
+
+	void assertIllegalArgument(const std::string& guessNum)
+	{
 		if (guessNum.length() != 3) {
 			throw length_error("3자리 숫자여야 합니다.");
 		}
 
 		for (char ch : guessNum) {
-			if (ch < '0' || ch > '9') {
-				throw invalid_argument("숫자여야 합니다.");
-			}
+			if (ch < '0' || ch > '9') continue;
+			throw invalid_argument("숫자여야 합니다.");
 		}
 
-		if (guessNum[0] == guessNum[1] || guessNum[1] == guessNum[2] || guessNum[0] == guessNum[2])
+		if (isThereDuplicatedNumber(guessNum))
 			throw invalid_argument("숫자가 중복되면 안됩니다.");
+	}
+
+	bool isThereDuplicatedNumber(const std::string& guessNum)
+	{
+		return guessNum[0] == guessNum[1] || guessNum[1] == guessNum[2] || guessNum[0] == guessNum[2];
 	}
 };
