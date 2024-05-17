8d45fe7bf305219499fab77e392b810dc28b679c
seungpyo.hong <hsp8405@naver.com>
2024-05-17 14:00:47 +0900

    [refactoring] 28

--- BaseBall/BaseBall.cpp
+++ BaseBall/BaseBall.cpp
@@ -6,6 +6,8 @@ using namespace std;
 class BaseBall {
 public:
 	void guess(const string& str) {
-		throw length_error("3자리 숫자여야 합니다.");
+		if (str.length() != 3) {
+			throw length_error("3자리 숫자여야 합니다.");
+		}
 	}
 };
